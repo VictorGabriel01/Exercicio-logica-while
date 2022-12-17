5c8a5780b8b0711f41c3b4f3177f6c2c9fda3f37
victorgabriel <112019336+VictorGabriel01@users.noreply.github.com>
2022-12-17 11:48:42 -0300

    Delete Exercício javascript while directory

--- Exercício javascript while/js/script2.js
+++
@@ -1,7 +0,0 @@
-var contadora = 1;
-var soma = 0;
-while (contadora < 101) {
-    var soma = soma+contadora;
-    contadora++;
-}
-alert("O valor da soma corresponde a: "+ soma);
--- Exercício javascript while/js/script4.js
+++
@@ -1,7 +0,0 @@
-var contadora = 0;
-while (contadora < 21) {
-    if (contadora % 2 == 1) {
-    alert(contadora+ " é impar");
-}
-contadora++;
-}
